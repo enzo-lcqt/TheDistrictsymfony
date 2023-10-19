76526789c768ea4b5df2c861e9467bee0c02a88a
[enzo] <[enzolocquet@gmail.com]>
2023-10-19 15:04:15 +0200

    ds

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,6 +7,7 @@ use App\Repository\DiscRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccueilController extends AbstractController
 {
@@ -14,25 +15,28 @@ class AccueilController extends AbstractController
 
     private $artistRepo;
     private $discRepo;
+    private $em;
 
-    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo)
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo, EntityManagerInterface $em)
     {
         $this->artistRepo = $artistRepo;
         $this->discRepo = $discRepo;
+        $this->em = $em;
 
     }
-
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
+       //on appelle le repository pour accéder à la fonction
+        $artistes = $this->artistRepo->getSomeArtists("Damso");
 
+        //on teste le contenu de la variable $artistes : dd() veut dire Dump and Die
+        dd($artistes); 
 
-        $artistes = $this->artistRepo->findAll();
+     // ...    
 
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-            'artistes' => $artistes
-        ]);
     }
+
+    
 
 }
