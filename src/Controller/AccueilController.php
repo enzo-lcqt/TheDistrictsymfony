90739749ef050e19782672da6c9ecf4e30b908c0
[enzo] <[enzolocquet@gmail.com]>
2023-10-19 09:20:57 +0200

    dq

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtistRepository;
+use App\Repository\DiscRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
+    
+
+    private $artistRepo;
+    private $discRepo;
+
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo)
+    {
+        $this->artistRepo = $artistRepo;
+        $this->discRepo = $discRepo;
+
+    }
+
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
+
+
+        $artistes = $this->artistRepo->findAll();
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'artistes' => $artistes
         ]);
     }
+
 }
